0f3452eba0864dabfcf27c76a3efb578e7c3ed8a
Nanak <nanakbandyopadhyay@hotmail.com>
2023-05-19 09:26:23 +0530

    make the card component more dynamic to accept params

--- components/Card/index.jsx
+++ components/Card/index.jsx
@@ -1,6 +1,11 @@
-export default function Card({ children }) {
-    return (
-      <div className="bg-tertiary shadow-sm rounded-md p-4 mb-6">{children}</div>
-    );
-  }
-  
+export default function Card({ children, noShadow, bgColor }) {
+  return (
+    <div
+      className={`${bgColor ? bgColor : "bg-tertiary"} ${
+        !noShadow ? "shadow-sm" : ""
+      } rounded-md p-4 mb-6`}
+    >
+      {children}
+    </div>
+  );
+}
