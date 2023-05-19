e609bb0a022d07696be07c3988b7277e0945a93f
Nanak <nanakbandyopadhyay@hotmail.com>
2023-05-19 20:47:10 +0530

    Change card default color

--- components/Card/index.jsx
+++ components/Card/index.jsx
@@ -1,9 +1,9 @@
 export default function Card({ children, noShadow, bgColor }) {
   return (
     <div
-      className={`${bgColor ? bgColor : "bg-tertiary"} ${
+      className={`${bgColor ? bgColor : "bg-tertiary300"} ${
         !noShadow ? "shadow-sm" : ""
-      } rounded-md p-4 mb-6`}
+      } rounded-md p-6 mb-6`}
     >
       {children}
     </div>
