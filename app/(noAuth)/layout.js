657614afa456c9374ece10f638a8cc4abfd4a654
Nanak <nanakbandyopadhyay@hotmail.com>
2023-05-22 08:31:28 +0530

    modify app navigation and add login page

---
+++ app/(noAuth)/layout.js
@@ -0,0 +1,14 @@
+import "../global.css";
+
+import { Ubuntu } from "next/font/google";
+
+const ubuntu = Ubuntu({ subsets: ["latin"], weight: "400" });
+
+export const metadata = {
+  title: "Connectopia",
+  description: "Stay connected",
+};
+
+export default function AutoLayout({ children }) {
+  return <div className="mt-4 max-w-6xl mx-auto">HI</div>;
+}
