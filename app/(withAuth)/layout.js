a7ec73c500766194b5aafbb48a755632b6f08c9b
Nanak <nanakbandyopadhyay@hotmail.com>
2023-05-23 21:14:01 +0530

    make root layout mobile responsive

--- app/(withAuth)/layout.js
+++ app/(withAuth)/layout.js
@@ -14,13 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body suppressHydrationWarning={true} className={ubuntu.className}>
-        <div className="mt-4 max-w-6xl mx-auto">
-          <div className="flex gap-6">
-            <div className="w-1/4">
-              <Navigation />
-            </div>
-            <div className="w-3/4">{children}</div>
+        <div className="mt-4 max-w-6xl mx-auto md:flex gap-6">
+          <div className="fixed md:static w-full bottom-0 md:w-1/4 -mb-6 z-10">
+            <Navigation />
           </div>
+          <div className="mx-4 md:mx-0 md:w-3/4">{children}</div>
         </div>
       </body>
     </html>
