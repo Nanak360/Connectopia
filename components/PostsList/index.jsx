9701e8bf96ff08a69614ecf9a02bf35d834bbdf3
Nanak <nanakbandyopadhyay@hotmail.com>
2023-06-21 09:48:26 +0530

    map posts in postsList component

--- components/PostsList/index.jsx
+++ components/PostsList/index.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Post from "@components/Post";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
-const PostsList = () => {
+const PostsList = ({}) => {
+  const [posts, setPosts] = useState([]);
+  const supabase = createClientComponentClient();
+  const getPosts = async () => {
+    supabase
+      .from("posts")
+      .select()
+      .then(({ data, error }) => {
+        if (!error) setPosts(data);
+      });
+  };
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return (
-    <div>
-      <Post />
-      <Post />
-      <Post />
-    </div>
+    <>
+      {posts.map((post) => (
+        <Post userId={post.author} postText={post.content} key={post.id} />
+      ))}
+    </>
   );
 };
 
