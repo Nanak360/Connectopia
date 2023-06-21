268f5844055da81ce5e6b1ed6844c5f7895d0d03
Nanak <nanakbandyopadhyay@hotmail.com>
2023-06-21 09:50:04 +0530

    make post component dynamic

--- components/Post/index.jsx
+++ components/Post/index.jsx
@@ -7,25 +7,23 @@ import PostReaction from "./PostReaction";
 import PostComment from "./PostComment";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
-const Post = () => {
+const Post = ({ userId, imgLink, postText, postCreationTime }) => {
   const supabase = createClientComponentClient();
   const [userAvatar, setUserAvatar] = useState("");
   const [userName, setUserName] = useState("");
   async function fetchSession() {
-    const { data } = await supabase.auth.getSession();
-    if (data?.session) {
-      supabase
-        .from("profiles")
-        .select()
-        .eq("id", data.session.user.id)
-        .then(({ data }) => {
-          setUserAvatar(data[0]?.avatar);
-          setUserName(data[0]?.name);
-        });
-    }
+    supabase
+      .from("profiles")
+      .select()
+      .eq("id", userId)
+      .then(({ data }) => {
+        console.log(data);
+        setUserAvatar(data[0]?.avatar);
+        setUserName(data[0]?.name);
+      });
   }
   useEffect(() => {
-    fetchSession();
+    if (userId) fetchSession();
   }, []);
   return (
     <Card>
@@ -34,11 +32,7 @@ const Post = () => {
         userImage={userAvatar}
         relativePostTime={"Two hours ago"}
       />
-      <PostContent
-        imgLink={
-          "https://images.unsplash.com/photo-1521080755838-d2311117f767?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1262&q=80"
-        }
-      />
+      <PostContent imgLink={imgLink} content={postText} />
       <PostReaction likes={"12k"} comments={3212} shares={432} />
       <PostComment />
     </Card>
