85062f569d7ed728a2d7d7c527529e76615a4b36
Nanak <nanakbandyopadhyay@hotmail.com>
2023-06-21 08:58:59 +0530

    add create post functionality

--- components/PostFormCard/index.jsx
+++ components/PostFormCard/index.jsx
@@ -1,24 +1,46 @@
+"use client";
 import Avatar from "@components/Avatar";
 import Card from "@components/Card";
 import HorizontalIconsPanel from "@components/HorizontalIconsPanel";
-import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import { Send } from "react-feather";
 import { createPostOptions } from "@constants";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 const PostFormCard = () => {
+  const supabase = createClientComponentClient();
+  const [content, setContent] = useState("");
+  const createPost = async () => {
+    const { data: sessionData } = await supabase.auth.getSession();
+    if (sessionData)
+      await supabase
+        .from("posts")
+        .insert({
+          author: sessionData.session.user.id,
+          content,
+        })
+        .then(({ data, error }) => {
+          if (!error) setContent("");
+          alert("Post created");
+        });
+  };
   return (
     <Card>
       <div className="flex gap-4">
         <Avatar></Avatar>
         <textarea
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
           className="grow mt-1 bg-transparent"
           placeholder={`What's on your mind?`}
         />
       </div>
       <div className="flex items-center justify-between mt-2">
         <HorizontalIconsPanel options={createPostOptions} isButton={true} />
-        <button className="flex items-center gap-1 bg-socialBlue300 hover:bg-socialBlue400 rounded-3xl px-6 py-2 text-sm place-self-end">
+        <button
+          onClick={createPost}
+          className="flex items-center gap-1 bg-socialBlue300 hover:bg-socialBlue400 rounded-3xl px-6 py-2 text-sm place-self-end"
+        >
           <Send size={16} />
           Share
         </button>
