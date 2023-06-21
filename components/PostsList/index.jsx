8554ce9fb8158613fae7af435269f9240880a276
Nanak <nanakbandyopadhyay@hotmail.com>
2023-06-21 09:01:45 +0530

    add posts list component

---
+++ components/PostsList/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Post from "@components/Post";
+
+const PostsList = () => {
+  return (
+    <div>
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
+};
+
+export default PostsList;
