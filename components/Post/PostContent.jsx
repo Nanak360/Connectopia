5b9b7e320e400bcb3e1a875e249a533ee24ad3e7
Nanak <nanakbandyopadhyay@hotmail.com>
2023-06-22 11:56:26 +0530

    modify posts card styles

--- components/Post/PostContent.jsx
+++ components/Post/PostContent.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const PostContent = ({ imgLink, content }) => {
   return (
     <div>
-      <p className="my-3 text-sm">{content}</p>
+      <p className="my-3 text-xl">{content}</p>
       {imgLink && (
         <div className="rounded-md overflow-hidden">
           <img src={imgLink} />
